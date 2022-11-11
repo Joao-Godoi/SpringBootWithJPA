e4808d8efff6fa4a82f97c4a4b96654dc69a815a
João Godoi <joaogodoi.dev@gmail.com>
2022-11-10 23:22:33 -0300

    :tada: Init: Project created

---
+++ src/test/java/com/joaogodoi/SpringBootWithJPA/SpringBootWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaogodoi.SpringBootWithJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
