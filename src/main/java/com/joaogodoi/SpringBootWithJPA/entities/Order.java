04f1f447b734012906027d2eaaf91929e217d4d8
João Godoi <joaogodoi.dev@gmail.com>
2022-11-15 17:12:14 -0300

    :sparkles: Feat: Created payment entity

--- src/main/java/com/joaogodoi/SpringBootWithJPA/entities/Order.java
+++ src/main/java/com/joaogodoi/SpringBootWithJPA/entities/Order.java
@@ -31,6 +31,9 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
@@ -77,6 +80,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
