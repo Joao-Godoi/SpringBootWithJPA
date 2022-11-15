f1493101416a2975379b1a029529bd695176b29f
João Godoi <joaogodoi.dev@gmail.com>
2022-11-15 16:08:31 -0300

    :sparkles: Feat: Created order item entity

--- src/main/java/com/joaogodoi/SpringBootWithJPA/entities/Order.java
+++ src/main/java/com/joaogodoi/SpringBootWithJPA/entities/Order.java
@@ -6,12 +6,18 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "td_order")
 public class Order implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    @OneToMany(mappedBy = "id.order")
+    private final Set<OrderItem> items = new HashSet<>();
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -66,6 +72,10 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 
     @Override
